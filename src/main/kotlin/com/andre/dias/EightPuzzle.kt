defe60f4de4afe44d8f437c6ac4a0f6912594987
andré <andredds12@gmail.com>
2021-02-21 21:39:46 -0300

    improving code

--- src/main/kotlin/com/andre/dias/EightPuzzle.kt
+++ src/main/kotlin/com/andre/dias/EightPuzzle.kt
@@ -9,21 +9,24 @@ import com.andre.dias.Movement.LEFT
 import com.andre.dias.Movement.RIGHT
 import com.andre.dias.Movement.UP
 import com.andre.dias.Util.Companion.show
+import kotlin.math.abs
 
 typealias PuzzleList = List<MutableList<Int?>>
 
-class EightPuzzle {
-    private val CORRECT_POSITIONS_MAX = 9
-    private val SELECTED_ALGORITHM = UNIFORM_COST
+private const val CORRECT_POSITIONS_MAX = 9
+private val SELECTED_ALGORITHM = UNIFORM_COST
 
-    private var maxPoints = 0
-    private var maxFronteira = 0
+class EightPuzzle {
 
     private val initialState: PuzzleList = listOf(
-        mutableListOf(8, 7, 1),
-        mutableListOf(6, null, 2),
-        mutableListOf(5, 4, 3)
+        mutableListOf(null, 1, 2),
+        mutableListOf(4, 5, 3),
+        mutableListOf(7, 8, 6)
     )
+
+    //    mutableListOf(8, 7, 1),
+    //    mutableListOf(6, null, 2),
+    //    mutableListOf(5, 4, 3)
 
     private val goalState: PuzzleList = listOf(
         mutableListOf(1, 2, 3),
@@ -33,72 +36,16 @@ class EightPuzzle {
 
     private val exploredStates: MutableList<State> = mutableListOf()
     private var openStates: MutableList<State> =
-        mutableListOf(State(initialState, calculateCost(initialState), mutableListOf()))
+        mutableListOf(
+            State(
+                initialState,
+                calculateCost(initialState)
+            )
+        )
 
-    private fun availableMovements(x: Int, y: Int): List<Movement> = when {
-        x == 0 && y == 0 -> listOf(RIGHT, DOWN)
-        x == 1 && y == 0 -> listOf(LEFT, RIGHT, DOWN)
-        x == 2 && y == 0 -> listOf(LEFT, DOWN)
-        x == 0 && y == 1 -> listOf(UP, RIGHT, DOWN)
-        x == 1 && y == 1 -> listOf(UP, DOWN, RIGHT, LEFT)
-        x == 2 && y == 1 -> listOf(UP, LEFT, DOWN)
-        x == 0 && y == 2 -> listOf(UP, RIGHT)
-        x == 1 && y == 2 -> listOf(UP, LEFT, RIGHT)
-        x == 2 && y == 2 -> listOf(UP, LEFT)
-        else -> throw Exception("unknown position")
-    }
-
-    private fun isGoal(state: PuzzleList): Boolean {
-        maxPoints = calculateCost(state)
-        return maxPoints == CORRECT_POSITIONS_MAX
-    }
-
-    private fun calculateCost(state: PuzzleList): Int {
-        return when (SELECTED_ALGORITHM) {
-            UNIFORM_COST -> uniformCost(state)
-            A_HEURISTIC_SIMPLE -> TODO()
-            A_HEURISTIC_BEST -> TODO()
-        }
-    }
-
-    private fun uniformCost(state: PuzzleList): Int {
-        return state.mapIndexed { index, row ->
-            val rowGoal = goalState[index]
-            row.mapIndexed { indexRow, item ->
-                if (rowGoal[indexRow] == item) 1 else 0
-            }.sum()
-        }.sum()
-    }
-
-    private fun findActualPosition(state: PuzzleList): Pair<Int?, Int?> {
-        state.forEachIndexed { index, row ->
-            row.forEachIndexed { indexRow, item ->
-                if (item == null) return Pair(indexRow, index)
-            }
-        }
-        return Pair(null, null)
-    }
-
-    private fun exists(list: List<State>, state: State) = list.any { es -> es.toString() == state.toString() }
-
-    private fun Movement.applyMovement(
-        newState: PuzzleList,
-        actualEmptyPosition: Pair<Int, Int>,
-        pastMovements: List<State>
-    ): State {
-        val (x, y) = actualEmptyPosition
-
-        val newX = x + this.x
-        val newY = y + this.y
-
-        val actualPosition = newState[y][x]
-        val newPosition = newState[newY][newX]
-
-        newState[y][x] = newPosition
-        newState[newY][newX] = actualPosition
-
-        return State(newState, calculateCost(newState), pastMovements)
-    }
+    private var maxPoints = 0
+    private var maxFronteira = 0
+    private var finalState: State? = null
 
     fun start() {
         println("-----------------------------------");
@@ -121,7 +68,10 @@ class EightPuzzle {
             openStates.remove(actualState)
 
             val goal = isGoal(state)
-            if (goal) break
+            if (goal) {
+                finalState = actualState
+                break
+            }
 
             val (x, y) = findActualPosition(state)
 
@@ -130,40 +80,127 @@ class EightPuzzle {
 
             val availableMovements: List<Movement> = availableMovements(x, y)
 
-            availableMovements.generateMovements(state, movements + actualState, x to y)
+            availableMovements.generateMovements(actualState, movements + actualState, x to y)
 
-            openStates = openStates.sortedByDescending(State::cost).toMutableList()
+            openStates = openStates
+                .sortedBy(State::cost)
+                .toMutableList()
         }
 
         showResult()
     }
 
+    private fun exists(list: List<State>, state: State) = list.any { es -> es.toString() == state.toString() }
+
     private fun checkFronteiraAndUpdate() {
         if (openStates.size > maxFronteira) maxFronteira = openStates.size
     }
 
+    private fun isGoal(state: PuzzleList): Boolean {
+        maxPoints = CORRECT_POSITIONS_MAX - incorrectPositions(state)
+        return maxPoints == CORRECT_POSITIONS_MAX
+    }
+
+    private fun findActualPosition(state: PuzzleList, of: Int? = null): Pair<Int?, Int?> {
+        state.forEachIndexed { index, row ->
+            row.forEachIndexed { indexRow, item ->
+                if (item == of) return Pair(indexRow, index)
+            }
+        }
+        return Pair(null, null)
+    }
+
+    private fun availableMovements(x: Int, y: Int): List<Movement> = when {
+        x == 0 && y == 0 -> listOf(RIGHT, DOWN)
+        x == 1 && y == 0 -> listOf(LEFT, RIGHT, DOWN)
+        x == 2 && y == 0 -> listOf(LEFT, DOWN)
+        x == 0 && y == 1 -> listOf(UP, RIGHT, DOWN)
+        x == 1 && y == 1 -> listOf(UP, DOWN, RIGHT, LEFT)
+        x == 2 && y == 1 -> listOf(UP, LEFT, DOWN)
+        x == 0 && y == 2 -> listOf(UP, RIGHT)
+        x == 1 && y == 2 -> listOf(UP, LEFT, RIGHT)
+        x == 2 && y == 2 -> listOf(UP, LEFT)
+        else -> throw Exception("unknown position")
+    }
+
+    private fun List<Movement>.generateMovements(
+        state: State, pastMovements: List<State>, actualEmptyPosition: Pair<Int, Int>
+    ) {
+        this.map { movement -> movement.applyMovement(state, actualEmptyPosition, pastMovements) }
+            .filterNot((::exists)(openStates))
+            .forEach(openStates::add)
+    }
+
+    private fun Movement.applyMovement(
+        state: State,
+        actualEmptyPosition: Pair<Int, Int>,
+        pastMovements: List<State>
+    ): State {
+        val (actualState, cost) = state
+        val newState = actualState.copy()
+
+        val (x, y) = actualEmptyPosition
+
+        val newX = x + this.x
+        val newY = y + this.y
+
+        val actualPosition = newState[y][x]
+        val newPosition = newState[newY][newX]
+
+        newState[y][x] = newPosition
+        newState[newY][newX] = actualPosition
+
+        return State(
+            newState,
+            calculateCost(newState, cost),
+            pastMovements
+        )
+    }
+
+    private fun calculateCost(state: PuzzleList, pastMovement: Int = 0): Int = when (SELECTED_ALGORITHM) {
+        UNIFORM_COST -> pastMovement + 1
+        A_HEURISTIC_SIMPLE -> incorrectPositions(state)
+        A_HEURISTIC_BEST -> heuristicBest(state)
+    }
+
+    private fun incorrectPositions(state: PuzzleList) =
+        state.mapIndexed { y, row ->
+            val rowGoal = goalState[y]
+            row.mapIndexed { x, item ->
+                if (rowGoal[x] == item) 0 else 1
+            }.sum()
+        }.sum()
+
+    private fun heuristicBest(state: PuzzleList): Int {
+        return state.mapIndexed { y, row ->
+            val rowGoal = goalState[y]
+            row.mapIndexed { x, item ->
+                if (rowGoal[x] == item) 0 else {
+                    val value = state[y][x]
+                    val (xExcepted, yExpected) = findActualPosition(goalState, value)
+
+                    requireNotNull(xExcepted)
+                    requireNotNull(yExpected)
+
+                    abs(xExcepted - x) + abs(yExpected - y)
+                }
+            }.sum()
+        }.sum()
+    }
+
     private fun showResult() {
-        val finalState = openStates.first()
-        finalState.allStates.forEach {
+        requireNotNull(finalState)
+
+        finalState?.allStates?.forEach {
             it.show()
         }
 
-        finalState.show()
+        finalState?.show()
 
         println("O total de nodos visitados: ${exploredStates.size}")
         println("O total de nodos expandidos/criados: ${openStates.size}")
         println("O maior tamanho da fronteira durante a busca: $maxFronteira")
-        println("O tamanho do caminho: ${finalState.allStates.size} ")
-    }
-
-    private fun List<Movement>.generateMovements(
-        state: PuzzleList,
-        pastMovements: List<State>,
-        actualEmptyPosition: Pair<Int, Int>
-    ) {
-        this.map { movement -> movement.applyMovement(state.copy(), actualEmptyPosition, pastMovements) }
-            .filterNot((::exists)(openStates))
-            .forEach(openStates::add)
+        println("O tamanho do caminho: ${finalState?.allStates?.size} ")
     }
 
     private fun PuzzleList.copy() = toMutableList().map(List<Int?>::toMutableList)
--- src/main/kotlin/com/andre/dias/State.kt
+++ src/main/kotlin/com/andre/dias/State.kt
@@ -1,6 +1,10 @@
 package com.andre.dias
 
-data class State(val actualState: PuzzleList, val cost: Int, val allStates: List<State>) {
+data class State(
+    val actualState: PuzzleList,
+    val cost: Int,
+    val allStates: List<State> = mutableListOf()
+) {
     override fun toString(): String {
         return actualState.map {
             it.map { value ->
